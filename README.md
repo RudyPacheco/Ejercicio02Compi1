c7f775c29db4179d7005c938c59e11ff534aa0a0
Rudy Pacheco <rudypacheco201930220@cunoc.edu.gt>
2022-02-14 10:57:02 -0600

    Subiendo proyecto

---
+++ README.md
@@ -0,0 +1,1 @@
+# Ejercicio02Compi1
---
+++ src/main/java/com/mycompany/ejercicio02compi1/run.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package com.mycompany.ejercicio02compi1;
+
+import com.rudy.interfaz.ventanaPrincipal;
+
+/**
+ * 
+ * @author AndaryuS
+ */
+public class run {
+    public static void main(String[] args) {
+        ventanaPrincipal ventana = new ventanaPrincipal();
+        ventana.setVisible(true);
+    }
+}
